f69a11604c42d0f553359e27b53cc9a362ac6a31
“Piter” <piter1234@gmail.com>
2021-05-05 14:04:19 +0200

    initial commit

---
+++ src/main/java/pitertest/Animals.java
@@ -0,0 +1,15 @@
+package pitertest;
+
+public class Animals {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
